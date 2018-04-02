8b8fd7e8f39640990235b6d3e18351d03f63d1bf
Artem Muray <murikmail@gmail.com>
2018-04-02 11:52:13 +0100

    First commit

---
+++ app/app.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { AppComponent } from "./app.component";
+import { SecondComponent } from "./second.component";
+
+@NgModule(
+{
+    imports: [BrowserModule], 
+    declarations: [AppComponent, SecondComponent], 
+    bootstrap: [AppComponent, SecondComponent]
+})
+export class AppModule {
+
+}
---
+++ app/second.component.ts
@@ -0,0 +1,6 @@
+import {Component} from "@angular/core"
+
+@Component({ selector: "my-new-app", template: "<h2>Second line!!!</h2>" })
+export class SecondComponent {
+ 
+}
